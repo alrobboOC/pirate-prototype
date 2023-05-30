b5acd80985f80a21eaccb2b218568651ed385dfd
Lydia_V <lydia.valtind@opencastsoftware.com>
2023-05-30 15:43:42 +0100

    first commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,19 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/beard', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var beard = req.session.data['beard']
+
+  // Check whether the variable matches a condition
+  if (beard == "yes"){
+    // Send user to next page
+    res.redirect('/crimes')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/fake-beard')
+  }
+
+})
